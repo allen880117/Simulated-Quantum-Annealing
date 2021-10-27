d6478642f7ec0c457158aef30de5ca417f97fe78
allen880117 <wangallen880117@gmail.com>
2021-10-27 22:41:56 +0800

    Add Verision 7.2.c2

--- vitis/src/include/sqa.hpp
+++ vitis/src/include/sqa.hpp
@@ -10,7 +10,7 @@
 #define CUR_SYN 2
 
 #ifndef CUR_SIM
-#define CUR_SIM 3
+    #define CUR_SIM 3
 #endif
 
 #define COND(NUM)                                \
@@ -18,83 +18,83 @@
         (!(__SYNTHESIS__) && ((CUR_SIM) == (NUM)))
 
 #if COND(0)
-#define NUM_TROT 16
-#define NUM_SPIN 4096
-#define PACKET_SIZE 64
-#define LOG2_PACKET_SIZE 6
-#define NUM_STREAM 1
-#define LOG2_NUM_STREAM 0
-#define HALF_NUM_STREAM 0
-#define NUM_FADD 4
-#define COPYSIGNF 1
+    #define NUM_TROT 16
+    #define NUM_SPIN 4096
+    #define PACKET_SIZE 64
+    #define LOG2_PACKET_SIZE 6
+    #define NUM_STREAM 1
+    #define LOG2_NUM_STREAM 0
+    #define HALF_NUM_STREAM 0
+    #define NUM_FADD 4
+    #define COPYSIGNF 1
 
 #elif COND(1)
-#define NUM_TROT 4
-#define NUM_SPIN 4096
-#define PACKET_SIZE 128
-#define LOG2_PACKET_SIZE 7
-#define NUM_STREAM 8
-#define LOG2_NUM_STREAM 3
-#define HALF_NUM_STREAM 4
-#define NUM_FADD 64
-#define COPYSIGNF 1
+    #define NUM_TROT 4
+    #define NUM_SPIN 4096
+    #define PACKET_SIZE 128
+    #define LOG2_PACKET_SIZE 7
+    #define NUM_STREAM 8
+    #define LOG2_NUM_STREAM 3
+    #define HALF_NUM_STREAM 4
+    #define NUM_FADD 64
+    #define COPYSIGNF 1
 
 #elif COND(2)
-#define NUM_TROT 4
-#define NUM_SPIN 4096
-#define PACKET_SIZE 64
-#define LOG2_PACKET_SIZE 6
-#define NUM_STREAM 2
-#define LOG2_NUM_STREAM 1
-#define HALF_NUM_STREAM 1
-#define NUM_FADD 64
-#define COPYSIGNF 0
+    #define NUM_TROT 4
+    #define NUM_SPIN 4096
+    #define PACKET_SIZE 64
+    #define LOG2_PACKET_SIZE 6
+    #define NUM_STREAM 2
+    #define LOG2_NUM_STREAM 1
+    #define HALF_NUM_STREAM 1
+    #define NUM_FADD 64
+    #define COPYSIGNF 0
 
 #elif COND(3)
-#define NUM_TROT 4
-#define NUM_SPIN 32
-#define PACKET_SIZE 16
-#define LOG2_PACKET_SIZE 4
-#define NUM_STREAM 2
-#define LOG2_NUM_STREAM 1
-#define HALF_NUM_STREAM 1
-#define NUM_FADD 64
-#define COPYSIGNF 0
+    #define NUM_TROT 4
+    #define NUM_SPIN 32
+    #define PACKET_SIZE 16
+    #define LOG2_PACKET_SIZE 4
+    #define NUM_STREAM 2
+    #define LOG2_NUM_STREAM 1
+    #define HALF_NUM_STREAM 1
+    #define NUM_FADD 64
+    #define COPYSIGNF 0
 
 #elif COND(4)
-#define NUM_TROT 4
-#define NUM_SPIN 16
-#define PACKET_SIZE 16
-#define LOG2_PACKET_SIZE 4
-#define NUM_STREAM 1
-#define LOG2_NUM_STREAM 0
-#define HALF_NUM_STREAM 0
-#define NUM_FADD 64
-#define COPYSIGNF 0
+    #define NUM_TROT 4
+    #define NUM_SPIN 16
+    #define PACKET_SIZE 16
+    #define LOG2_PACKET_SIZE 4
+    #define NUM_STREAM 1
+    #define LOG2_NUM_STREAM 0
+    #define HALF_NUM_STREAM 0
+    #define NUM_FADD 64
+    #define COPYSIGNF 0
 
 #else
-#define NUM_TROT 8
-#define NUM_SPIN 1024
-#define PACKET_SIZE 16
-#define LOG2_PACKET_SIZE 4
-#define NUM_STREAM 4
-#define LOG2_NUM_STREAM 2
-#define HALF_NUM_STREAM 2
-#define NUM_FADD 16
-#define COPYSIGNF 0
+    #define NUM_TROT 8
+    #define NUM_SPIN 1024
+    #define PACKET_SIZE 16
+    #define LOG2_PACKET_SIZE 4
+    #define NUM_STREAM 4
+    #define LOG2_NUM_STREAM 2
+    #define HALF_NUM_STREAM 2
+    #define NUM_FADD 16
+    #define COPYSIGNF 0
 
 #endif
 
 typedef unsigned int u32_t;
-typedef int i32_t;
+typedef int          i32_t;
 
-typedef float fp_t;
+typedef float      fp_t;
 typedef ap_uint<1> spin_t;
 typedef struct {
     fp_t data[PACKET_SIZE];
 } fp_pack_t;
 typedef ap_uint<PACKET_SIZE * NUM_STREAM> spin_pack_t;
-typedef ap_uint<PACKET_SIZE> spin_pack_u50_t;
+typedef ap_uint<PACKET_SIZE>              spin_pack_u50_t;
 
 /* Quantum Monte-Carlo */
 void QuantumMonteCarlo(
@@ -117,7 +117,8 @@ void QuantumMonteCarloU50(
     /* Spins */
     spin_pack_u50_t trotters[NUM_TROT][NUM_SPIN / PACKET_SIZE],
     /* Jcoup */
-    const fp_pack_t Jcoup[NUM_SPIN][NUM_SPIN / PACKET_SIZE],
+    const fp_pack_t Jcoup_0[NUM_SPIN][NUM_SPIN / PACKET_SIZE / NUM_STREAM],
+    const fp_pack_t Jcoup_1[NUM_SPIN][NUM_SPIN / PACKET_SIZE / NUM_STREAM],
     /* Array of h */
     const fp_t h[NUM_SPIN],
     /* Thermal Related */
@@ -130,7 +131,7 @@ void QuantumMonteCarloU50(
 
 /* Naive Version */
 void QuantumMonteCarloBasic(const int nTrot, const int nSpin,
-                            spin_t trotters[NUM_TROT][NUM_SPIN],
+                            spin_t     trotters[NUM_TROT][NUM_SPIN],
                             const fp_t Jcoup[NUM_SPIN][NUM_SPIN],
                             const fp_t h[NUM_SPIN], const fp_t Jperp,
                             const fp_t Beta,
